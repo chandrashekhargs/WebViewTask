1733a34c082c1abda809bd590be5ef0af12434f2
chandrashekhar <gs.chandu09@gmail.com>
2016-11-18 09:19:53 +0530

    Initial commit

---
+++ app/src/main/java/com/techpalle/webviewtask/MainActivity.java
@@ -0,0 +1,28 @@
+package com.techpalle.webviewtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wb;
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wb = (WebView) findViewById(R.id.webView1);
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String view = "http://www.skillgun.com";
+                wb.getSettings().setJavaScriptEnabled(true);
+                wb.loadUrl(view);
+            }
+        });
+
+    }
+}
